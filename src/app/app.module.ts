955f1b687f6a2c3a8d8c4eb628fed9c94ed37fa0
Ousmane Ndiaye <ousmane.ndiaye6@orange-sonatel.com>
2020-01-21 11:07:59 +0000

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentLayoutComponent,
+    AuthLayoutComponent
   ],
   imports: [
     BrowserModule,
